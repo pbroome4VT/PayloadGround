09585ef00b507a199f3f56e8346f978068c941c3
pbroome4VT <89484743+pbroome4VT@users.noreply.github.com>
2022-04-16 14:25:44 -0400

    updated windows bat to have a delay
    prevent it from writing data from previous runs

--- windowsPayload.bat
+++ windowsPayload.bat
@@ -4,5 +4,10 @@ SET PAYLOAD_DIR=%~dp0
 :: So we can now use relative paths
 cd %PAYLOAD_DIR%
 
+::start reciever
 START "reciever.py" python3 reciever.py
+
+::delay one second so reciever has time to clear old coords.txt
+TIMEOUT 1
+
 START "writer.py" python3 writer.py
